0ce69b729874e1bcc7afd5d8fb738e6ca18aacb3
Hyhello <673089899@qq.com>
2018-04-02 22:42:45 +0800

    修改编码模式

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -24,6 +24,14 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent': 0,
+    'eol-last': 0,
+    'no-tabs': 0,
+    "one-var": 0,
+    "no-mixed-spaces-and-tabs": 0,
+    "semi": ["error", "always"],
+    "no-useless-return": 0,
+    "no-multi-spaces": ["error", { ignoreEOLComments: true }]
   }
 }
